1daf24b9cab7323ff00a2be4dfb7f4a49ed06c25
Mohammadhassan <bagheri.mh69@gmail.com>
2025-08-04 22:34:24 +0200

    feat: Add prompt to install app

---
+++ public/src/js/app.js
@@ -0,0 +1,11 @@
+var deferredPrompt;
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js").then(function () {
+    console.log("Service worker registered!");
+  });
+}
+
+window.addEventListener("beforeinstallprompt", function (event) {
+  event.preventDefault();
+  deferredPrompt = event;
+});
